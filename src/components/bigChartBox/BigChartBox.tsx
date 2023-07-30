f17b013ca2936c06bc77544aeddd01bf04600f4a
Chiran Jeewantha <chiranjonline@gmail.com>
2023-07-30 23:42:41 +0530

    #8 react dashboard animated charts

---
+++ src/components/bigChartBox/BigChartBox.tsx
@@ -0,0 +1,77 @@
+import { Area, AreaChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
+import './bigChartBox.scss'
+
+const data = [
+    {
+        name: 'Sun',
+        books: 4000,
+        clothes: 2400,
+        electronics: 2400,
+    },
+    {
+        name: 'Mon',
+        books: 3000,
+        clothes: 1398,
+        electronics: 2210,
+    },
+    {
+        name: 'Tue',
+        books: 2000,
+        clothes: 9800,
+        electronics: 2290,
+    },
+    {
+        name: 'Wen',
+        books: 2780,
+        clothes: 3908,
+        electronics: 2000,
+    },
+    {
+        name: 'Thurs',
+        books: 4000,
+        clothes: 2300,
+        electronics: 2400,
+    },
+    {
+        name: 'Fri',
+        books: 3900,
+        clothes: 2400,
+        electronics: 1400,
+    },
+    {
+        name: 'Sat',
+        books: 4000,
+        clothes: 3000,
+        electronics: 2400,
+    }
+  ];
+
+const BigChartBox = () => {
+  return (
+    <div className='bigChartBox'>
+        <h1>Revenue Analytics</h1>
+        <div className="chart">
+        <ResponsiveContainer width="99%" height="100%">
+        <AreaChart
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Area type="monotone" dataKey="electronics" stackId="1" stroke="#8884d8" fill="#8884d8" />
+          <Area type="monotone" dataKey="clothes" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
+          <Area type="monotone" dataKey="books" stackId="1" stroke="#ffc658" fill="#ffc658" />
+        </AreaChart>
+      </ResponsiveContainer>
+        </div>
+    </div>
+  )
+}
+
+export default BigChartBox
