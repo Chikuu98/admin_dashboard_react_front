46a4611d6c3ebfa8218dae6a5ee9ece6d7860a6b
Chiran Jeewantha <chiranjonline@gmail.com>
2023-08-03 19:46:20 +0530

    #10 react modal

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -2,6 +2,8 @@ import { GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
 import DataTable from "../../components/dataTable/DataTable"
 import "./users.scss"
 import { userRows } from "../../data";
+import { useState } from "react";
+import Add from "../../components/add/Add";
 
 const columns: GridColDef[] = [
   { field: 'id',
@@ -10,7 +12,7 @@ const columns: GridColDef[] = [
   editable:false
   },
   {
-    field: 'avatar',
+    field: 'img',
     headerName: 'Avatar',
     width: 100,
     renderCell: (params) => {
@@ -62,14 +64,16 @@ const columns: GridColDef[] = [
 ];
 
 const Users = () => {
+  const [open,setOpen] = useState(false);
 
   return (
     <div className="users">
       <div className="info">
         <h1>Users</h1>
-        <button>Add New User</button>
+        <button onClick={()=>setOpen(true)}>Add New User</button>
       </div>
       <DataTable slug="users"  columns={columns} rows= {userRows}/>
+      {open && <Add slug="user" columns={columns} setOpen={setOpen} />}
     </div>
   )
 }
